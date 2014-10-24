6d463bda9da28f19b3862f231aa05c98439d5386
spkkris <krystianek18@tlen.pl>
2014-10-24 23:40:54 +0200

    Administracja kompletna o ustawienia

--- infusions/download_center_panel/download_center_panel_admin.php
+++ infusions/download_center_panel/download_center_panel_admin.php
@@ -36,10 +36,10 @@ if (isset($_GET['status']) && !isset($message)) {
 	}
 	if ($message) {	echo "<div id='close-message'><div class='admin-message'>".$message."</div></div>\n"; }
 	}
-/*if (isset($_POST['zapisz'])) {
+if (isset($_POST['zapisz'])) {
 			
-	if (is_numeric($_POST['statystyki']) &&  is_numeric($_POST['top_users']) && is_numeric($_POST['ip_users']) && is_numeric($_POST['admin']) && is_numeric($_POST['menu_user']) && is_numeric($_POST['dodaj'])) {
-	$result = dbquery("UPDATE ".DB_KMF_USER." SET statystyki='".(isNum($_POST['statystyki']) ? $_POST['statystyki'] : "0")."', top_users='".(isNum($_POST['top_users']) ? $_POST['top_users'] : "0")."', ip_users='".(isNum($_POST['ip_users']) ? $_POST['ip_users'] : "0")."', admin='".(isNum($_POST['admin']) ? $_POST['admin'] : "0")."', menu_user='".(isNum($_POST['menu_user']) ? $_POST['menu_user'] : "0")."', dodaj='".(isNum($_POST['dodaj']) ? $_POST['dodaj'] : "0")."'");	
+	if (is_numeric($_POST['ile']) &&  is_numeric($_POST['slider'])) {
+	$result = dbquery("UPDATE ".DB_KMF_DCP." SET ile='".(isNum($_POST['ile']) ? $_POST['ile'] : "0")."', slider='".(isNum($_POST['slider']) ? $_POST['slider'] : "0")."'");	
 	if (!$result) { 
 	redirect(FUSION_SELF.$aidlink."&status=nsu");
 	} else {
@@ -49,63 +49,29 @@ if (isset($_GET['status']) && !isset($message)) {
 	redirect(FUSION_SELF.$aidlink."&status=nsue");
 	} 
 	}
-$kmfu_ustawienia = dbarray(dbquery("SELECT * FROM ".DB_KMF_USER));
-	$statystyki = isNum($kmfu_ustawienia['statystyki']);
-	$top_users = isNum($kmfu_ustawienia['top_users']);
-	$ip_users = isNum($kmfu_ustawienia['ip_users']);
-	$admin = isNum($kmfu_ustawienia['admin']);
-	$menu_user = isNum($kmfu_ustawienia['menu_user']);
-	$dodaj = isNum($kmfu_ustawienia['dodaj']);
+$kmfu_ustawienia = dbarray(dbquery("SELECT * FROM ".DB_KMF_DCP));
+	$ile = isNum($kmfu_ustawienia['ile']);
+	$slider = isNum($kmfu_ustawienia['slider']);
+	;
 opentable($locale['admin']);
-echo"<div style='text-align: center;' class='admin-message center'>".$locale['admin'].$locale['admin1']."</div>";
+echo"<div style='text-align: center;' class='admin-message center'>".$locale['admin']."</div>";
         echo "<form name='kmfu_form' method='post' action='".FUSION_SELF.$aidlink."'>\n";
 		echo "<table cellpadding='0' cellspacing='0' width='98%' style='margin-top: 10px;' class='tbl-border center'>\n<tr>\n";
 		echo "<td  class='tbl' width='50%' align='left'>".$locale['019']."</td>\n";
-		echo "<td  align='left' class='tbl'><select name='statystyki' class='textbox'>\n";
-		echo "<option value='1'".($kmfu_ustawienia['statystyki'] == "1" ? " selected='selected'" : "").">".$locale['wlacz']."</option>\n";
-		echo "<option value='0'".($kmfu_ustawienia['statystyki'] == "0" ? " selected='selected'" : "").">".$locale['wylacz']."</option>\n";
-		echo "</select>";
-		echo ($kmfu_ustawienia['statystyki'] == 1 ? "<img src='".INFUSIONS."kris_user_panel/img/on.png' alt='".$locale['wlacz']."' class='admin-icons'/>" : "<img src='".INFUSIONS."kris_user_panel/img/off.png' alt='".$locale['wylacz']."' class='admin-icons'/>");
+		echo "<td  align='left' class='tbl'><input name='ile' type='number' min='1' max='255' style='width: 50px' value='".$kmfu_ustawienia['ile']."' class='textbox'>\n";
+		echo "</input>* Max. 255";
 		echo "</td></tr><tr>\n";
 		echo "<td  class='tbl' width='50%' align='left'>".$locale['023']."</td>\n";
-		echo "<td  align='left' class='tbl'><select name='top_users' class='textbox'>\n";
-		echo "<option value='1'".($kmfu_ustawienia['top_users'] == "1" ? " selected='selected'" : "").">".$locale['wlacz']."</option>\n";
-		echo "<option value='0'".($kmfu_ustawienia['top_users'] == "0" ? " selected='selected'" : "").">".$locale['wylacz']."</option>\n";
+		echo "<td  align='left' class='tbl'><select name='slider' class='textbox'>\n";
+		echo "<option value='1'".($kmfu_ustawienia['slider'] == "1" ? " selected='selected'" : "").">".$locale['wlacz']."</option>\n";
+		echo "<option value='0'".($kmfu_ustawienia['slider'] == "0" ? " selected='selected'" : "").">".$locale['wylacz']."</option>\n";
 		echo "</select>";
-		echo ($kmfu_ustawienia['top_users'] == 1 ? "<img src='".INFUSIONS."kris_user_panel/img/on.png' alt='".$locale['wlacz']."' class='admin-icons'/>" : "<img src='".INFUSIONS."kris_user_panel/img/off.png' alt='".$locale['wylacz']."' class='admin-icons'/>");
+		echo ($kmfu_ustawienia['slider'] == 1 ? "<img src='".INFUSIONS."kris_user_panel/img/on.png' alt='".$locale['wlacz']."' class='admin-icons'/>" : "<img src='".INFUSIONS."kris_user_panel/img/off.png' alt='".$locale['wylacz']."' class='admin-icons'/>");
 		echo "</td></tr><tr>\n";
-		echo "<td  class='tbl' width='50%' align='left'>".$locale['020']."</td>\n";
-		echo "<td  align='left' class='tbl'><select name='ip_users' class='textbox'>\n";
-		echo "<option value='1'".($kmfu_ustawienia['ip_users'] == "1" ? " selected='selected'" : "").">".$locale['wlacz']."</option>\n";
-		echo "<option value='0'".($kmfu_ustawienia['ip_users'] == "0" ? " selected='selected'" : "").">".$locale['wylacz']."</option>\n";
-		echo "</select>";
-		echo ($kmfu_ustawienia['ip_users'] == 1 ? "<img src='".INFUSIONS."kris_user_panel/img/on.png' alt='".$locale['wlacz']."' class='admin-icons'/>" : "<img src='".INFUSIONS."kris_user_panel/img/off.png' alt='".$locale['wylacz']."' class='admin-icons'/>");
-		echo "</td></tr><tr>\n";
-		echo "<td  class='tbl' width='50%' align='left'>".$locale['021']."</td>\n";
-		echo "<td  align='left' class='tbl'><select name='admin' class='textbox'>\n";
-		echo "<option value='1'".($kmfu_ustawienia['admin'] == "1" ? " selected='selected'" : "").">".$locale['wlacz']."</option>\n";
-		echo "<option value='0'".($kmfu_ustawienia['admin'] == "0" ? " selected='selected'" : "").">".$locale['wylacz']."</option>\n";
-		echo "</select>";
-		echo ($kmfu_ustawienia['admin'] == 1 ? "<img src='".INFUSIONS."kris_user_panel/img/on.png' alt='".$locale['wlacz']."' class='admin-icons'/>" : "<img src='".INFUSIONS."kris_user_panel/img/off.png' alt='".$locale['wylacz']."' class='admin-icons'/>");
-		echo "</td></tr><tr>\n";
-		echo "<td  class='tbl' width='50%' align='left'>".$locale['016']."</td>\n";
-		echo "<td  align='left' class='tbl'><select name='menu_user' class='textbox'>\n";
-		echo "<option value='1'".($kmfu_ustawienia['menu_user'] == "1" ? " selected='selected'" : "").">".$locale['wlacz']."</option>\n";
-		echo "<option value='0'".($kmfu_ustawienia['menu_user'] == "0" ? " selected='selected'" : "").">".$locale['wylacz']."</option>\n";
-		echo "</select>";
-		echo ($kmfu_ustawienia['menu_user'] == 1 ? "<img src='".INFUSIONS."kris_user_panel/img/on.png' alt='".$locale['wlacz']."' class='admin-icons'/>" : "<img src='".INFUSIONS."kris_user_panel/img/off.png' alt='".$locale['wylacz']."' class='admin-icons'/>");
-		echo "</td></tr><tr>\n";
-		echo "<td  class='tbl' width='50%' align='left'>".$locale['017']."</td>\n";
-		echo "<td  align='left' class='tbl'><select name='dodaj' class='textbox'>\n";
-		echo "<option value='1'".($kmfu_ustawienia['dodaj'] == "1" ? " selected='selected'" : "").">".$locale['wlacz']."</option>\n";
-		echo "<option value='0'".($kmfu_ustawienia['dodaj'] == "0" ? " selected='selected'" : "").">".$locale['wylacz']."</option>\n";
-		echo "</select>";
-		echo ($kmfu_ustawienia['dodaj'] == 1 ? "<img src='".INFUSIONS."kris_user_panel/img/on.png' alt='".$locale['wlacz']."' class='admin-icons'/>" : "<img src='".INFUSIONS."kris_user_panel/img/off.png' alt='".$locale['wylacz']."' class='admin-icons'/>");
-	echo "</td></tr><tr>\n";
 		echo"</table>\n";
 		echo "<div style='text-align:center;margin-top:10px;margin-bottom:10px;'><input type='submit' class='button' name='zapisz' value='".$locale['zapisz']."' /></div>\n";
 		echo "</form>\n";
 closetable();
-*/
+
 require_once THEMES."templates/footer.php";
 ?>
