f42c825148a108e072823e8d3415588150ae3b69
spkkris <krystianek18@tlen.pl>
2014-10-29 17:43:54 +0100

    inc

--- infusions/download_center_panel/inc.php
+++ infusions/download_center_panel/inc.php
@@ -18,7 +18,9 @@
 +--------------------------------------------------------*/
 if (!defined("IN_FUSION")) { die("Access Denied"); }
 function autor() {
-echo "<div align='right'><a href='http://www.krismods-fusion.pl' target='_blank' title='Copyright KriS Mods Fusion 2014'>©</a></div>";
+$www = "http://www.krismods-fusion.pl";
+$copy = "Copyright KriS Mods Fusion 2014";
+echo "<div align='right'><a href='".$www."' target='_blank' title='".$copy."'>©</a></div>";
 }
 
 ?>
